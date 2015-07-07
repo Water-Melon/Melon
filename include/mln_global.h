532aeafd0ccd836b6bc4dee5596eb048095bbaef
Water-Melon <melon-c@hotmail.com>
2015-07-07 20:54:03 +0800

    Version 1.2.4 Update.

--- include/mln_global.h
+++ include/mln_global.h
@@ -16,11 +16,6 @@
 #endif
 
 
-EXTERN mln_u32_t cr0;
-EXTERN mln_u32_t cr1;
-EXTERN mln_u32_t cr2;
-EXTERN mln_u32_t cr3;
-
 /*functions*/
 extern void mln_global_init(void);
 #endif
--- include/mln_ipc.h
+++ include/mln_ipc.h
@@ -5,7 +5,5 @@
 #ifndef __MLN_IPC_H
 #define __MLN_IPC_H
 #define M_IPC_CONF_RELOAD 1
-#define M_IPC_DISTRIBUTE_INIT 2
-#define M_IPC_1 3
 extern void mln_set_ipc_handlers(void);
 #endif
