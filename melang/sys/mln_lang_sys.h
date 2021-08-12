0e2fb2050aa74830bb227b174a723fa957557a3d
Water-Melon <melon-c@hotmail.com>
2021-08-12 22:49:12 +0800

    add array key diff function in melang

--- melang/sys/mln_lang_sys.h
+++ melang/sys/mln_lang_sys.h
@@ -7,9 +7,17 @@
 
 #include "mln_lang.h"
 
+typedef struct {
+    mln_lang_ctx_t   *ctx;
+    mln_tcp_conn_t    conn;
+    mln_s64_t         size_limit;
+    mln_s64_t         cur_size;
+} mln_lang_sys_exec_t;
+
 struct mln_sys_diff_s {
     mln_lang_array_t *dest;
     mln_lang_array_t *notin;
+    mln_u32_t         key;
 };
 
 extern int mln_lang_sys(mln_lang_ctx_t *ctx);
