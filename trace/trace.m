b5d10a1ac146bf217ce32402730c2f6cb01bea54
Water-Melon <melon-c@hotmail.com>
2023-02-08 11:58:47 +0800

    update conf-file and trace script

--- trace/trace.m
+++ trace/trace.m
@@ -12,7 +12,7 @@ while (1) {
     ret = Pipe('recv');
     if (ret) {
         for (i = 0; i < sys.size(ret); ++i) {
-            //sys.print(ret[i]);
+            sys.print(ret[i]);
         }
     } fi
     sys.msleep(1000);
