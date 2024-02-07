94ed0d4febdce9eb728e486978528c636730bb0e
Water-Melon <melon-c@hotmail.com>
2024-02-07 08:58:48 +0000

    refactor(list): use function templates to define functions

--- src/mln_list.c
+++ src/mln_list.c
@@ -5,17 +5,16 @@
 
 #include "mln_list.h"
 #include "mln_utils.h"
+#include "mln_func.h"
 
 MLN_CHAIN_FUNC_DECLARE(mln_list, mln_list_t, static inline void,);
 MLN_CHAIN_FUNC_DEFINE(mln_list, mln_list_t, static inline void, prev, next);
 
-void mln_list_add(mln_list_t *sentinel, mln_list_t *node)
-{
+MLN_FUNC_VOID(, void, mln_list_add, (mln_list_t *sentinel, mln_list_t *node), (sentinel, node), {
     mln_list_chain_add(&mln_list_head(sentinel), &mln_list_tail(sentinel), node);
-}
+})
 
-void mln_list_remove(mln_list_t *sentinel, mln_list_t *node)
-{
+MLN_FUNC_VOID(, void, mln_list_remove, (mln_list_t *sentinel, mln_list_t *node), (sentinel, node), {
     mln_list_chain_del(&mln_list_head(sentinel), &mln_list_tail(sentinel), node);
-}
+})
 
