fa477728e6c527c7b2d4f7a7f7e9b4a91e836ddc
Water-Melon <melon-c@hotmail.com>
2024-02-08 04:18:23 +0000

    refactor(trace): use function templates to define functions

--- src/mln_trace.c
+++ src/mln_trace.c
@@ -6,6 +6,7 @@
 #include "mln_trace.h"
 #include "mln_conf.h"
 #include "mln_log.h"
+#include "mln_func.h"
 
 static mln_lang_t *trace_lang = NULL;
 static mln_lang_ctx_t *trace_ctx = NULL;
@@ -17,8 +18,7 @@ static int mln_trace_lang_clear(mln_lang_t *lang);
 static void mln_trace_lang_ctx_return_handler(mln_lang_ctx_t *ctx);
 static void mln_trace_lang_check(mln_event_t *ev, void *data);
 
-mln_string_t *mln_trace_path(void)
-{
+MLN_FUNC(, mln_string_t *, mln_trace_path, (void), (), {
     mln_conf_t *cf;
     mln_conf_domain_t *cd;
     mln_conf_cmd_t *cc;
@@ -40,10 +40,9 @@ mln_string_t *mln_trace_path(void)
         return ci->val.s;
 
     return NULL;
-}
+})
 
-int mln_trace_init(mln_event_t *ev, mln_string_t *path)
-{
+MLN_FUNC(, int, mln_trace_init, (mln_event_t *ev, mln_string_t *path), (ev, path), {
     if (path == NULL || trace_lang != NULL || trace_ctx != NULL) goto err1;
 
     if (socketpair(AF_UNIX, SOCK_STREAM, 0, trace_signal_fds) < 0) {
@@ -77,22 +76,20 @@ err2:
     close(trace_signal_fds[1]);
 err1:
     return -1;
-}
+})
 
 mln_lang_ctx_t *mln_trace_task_get(void)
 {
     return trace_ctx;
 }
 
-void mln_trace_finalize(void)
-{
+MLN_FUNC_VOID(, void, mln_trace_finalize, (void), (), {
     mln_lang_free(trace_lang);
     trace_lang = NULL;
     trace_ctx = NULL;
-}
+})
 
-static void mln_trace_lang_check(mln_event_t *ev, void *data)
-{
+MLN_FUNC_VOID(static, void, mln_trace_lang_check, (mln_event_t *ev, void *data), (ev, data), {
     if (trace_lang != NULL && trace_lang->run_head == NULL && trace_lang->wait_head == NULL) {
         mln_lang_free(trace_lang);
         trace_lang = NULL;
@@ -100,33 +97,28 @@ static void mln_trace_lang_check(mln_event_t *ev, void *data)
     } else {
         mln_event_timer_set(ev, 10, NULL, mln_trace_lang_check);
     }
-}
+})
 
-static int mln_trace_lang_signal(mln_lang_t *lang)
-{
+MLN_FUNC(static, int, mln_trace_lang_signal, (mln_lang_t *lang), (lang), {
     return mln_event_fd_set(mln_lang_event_get(lang), trace_signal_fds[0], M_EV_SEND|M_EV_ONESHOT, M_EV_UNLIMITED, lang, mln_lang_launcher_get(lang));
-}
+})
 
-static int mln_trace_lang_clear(mln_lang_t *lang)
-{
+MLN_FUNC(static, int, mln_trace_lang_clear, (mln_lang_t *lang), (lang), {
     return mln_event_fd_set(mln_lang_event_get(lang), trace_signal_fds[0], M_EV_CLR, M_EV_UNLIMITED, NULL, NULL);
-}
+})
 
-static void mln_trace_lang_ctx_return_handler(mln_lang_ctx_t *ctx)
-{
+MLN_FUNC_VOID(static, void, mln_trace_lang_ctx_return_handler, (mln_lang_ctx_t *ctx), (ctx), {
     trace_ctx = NULL;
     if (mln_lang_ctx_is_quit(ctx))
         mln_log(warn, "trace script exit\n");
-}
+})
 
-void mln_trace_init_callback_set(mln_trace_init_cb_t cb)
-{
+MLN_FUNC_VOID(, void, mln_trace_init_callback_set, (mln_trace_init_cb_t cb), (cb), {
     trace_init_cb = cb;
-}
+})
 
-int mln_trace_recv_handler_set(mln_lang_ctx_pipe_recv_cb_t recv_handler)
-{
+MLN_FUNC(, int, mln_trace_recv_handler_set, (mln_lang_ctx_pipe_recv_cb_t recv_handler), (recv_handler), {
     if (trace_ctx == NULL) return -1;
     return mln_lang_ctx_pipe_recv_handler_set(trace_ctx, recv_handler);
-}
+})
 
