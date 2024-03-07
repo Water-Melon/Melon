7e739f36225754570ddbd8a4378bcc1732694afc
Water-Melon <melon-c@hotmail.com>
2024-03-07 04:57:18 +0000

    feat(func): supports passing function arguments to entry and exit callback functions

--- include/mln_func.h
+++ include/mln_func.h
@@ -7,9 +7,49 @@
 
 #include <stdio.h>
 
-typedef void (*mln_func_cb_t)(const char *file, const char *func, int line);
+typedef void (*mln_func_cb_t)(const char *file, const char *func, int line, ...);
 
 #if defined(MLN_FUNC_FLAG)
+#if defined(MLN_C99)
+#define MLN_FUNC_STRIP(...) __VA_OPT__(,) __VA_ARGS__
+#define MLN_FUNC(scope, ret_type, name, params, args, ...) \
+    scope ret_type name params;\
+    scope ret_type __mln_func_##name params __VA_ARGS__\
+    scope ret_type name params {\
+        if (mln_func_entry != NULL) mln_func_entry(__FILE__, __FUNCTION__, __LINE__ MLN_FUNC_STRIP args);\
+        ret_type _r;\
+        _r = __mln_func_##name args;\
+        if (mln_func_exit != NULL) mln_func_exit(__FILE__, __FUNCTION__, __LINE__ MLN_FUNC_STRIP args);\
+        return _r;\
+    }
+#define MLN_FUNC_VOID(scope, ret_type, name, params, args, ...) \
+    scope ret_type name params;\
+    scope ret_type __mln_func_##name params __VA_ARGS__\
+    scope ret_type name params {\
+        if (mln_func_entry != NULL) mln_func_entry(__FILE__, __FUNCTION__, __LINE__ MLN_FUNC_STRIP args);\
+        __mln_func_##name args;\
+        if (mln_func_exit != NULL) mln_func_exit(__FILE__, __FUNCTION__, __LINE__ MLN_FUNC_STRIP args);\
+    }
+#define MLN_FUNC_CUSTOM(entry, exit, scope, ret_type, name, params, args, ...) \
+    scope ret_type name params;\
+    scope ret_type __mln_func_##name params __VA_ARGS__\
+    scope ret_type name params {\
+        entry(__FILE__, __FUNCTION__, __LINE__ MLN_FUNC_STRIP args);\
+        ret_type _r;\
+        _r = __mln_func_##name args;\
+        exit(__FILE__, __FUNCTION__, __LINE__ MLN_FUNC_STRIP args);\
+        return _r;\
+    }
+#define MLN_FUNC_VOID_CUSTOM(entry, exit, scope, ret_type, name, params, args, ...) \
+    scope ret_type name params;\
+    scope ret_type __mln_func_##name params __VA_ARGS__\
+    scope ret_type name params {\
+        entry(__FILE__, __FUNCTION__, __LINE__ MLN_FUNC_STRIP args);\
+        __mln_func_##name args;\
+        exit(__FILE__, __FUNCTION__, __LINE__ MLN_FUNC_STRIP args);\
+    }
+
+#else
 #define MLN_FUNC(scope, ret_type, name, params, args, ...) \
     scope ret_type name params;\
     scope ret_type __mln_func_##name params __VA_ARGS__\
@@ -46,6 +86,7 @@ typedef void (*mln_func_cb_t)(const char *file, const char *func, int line);
         __mln_func_##name args;\
         exit(__FILE__, __FUNCTION__, __LINE__);\
     }
+#endif
 #else
 #define MLN_FUNC(scope, ret_type, name, params, args, ...) \
     scope ret_type name params __VA_ARGS__
