4f015b599d00c7e46b46430b75649679cb54bd0e
Water-Melon <melon-c@hotmail.com>
2024-04-24 14:44:52 +0000

    chore(tests): fix file set test

--- t/file.c
+++ t/file.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "mln_file.h"
+#include <assert.h>
 
 int main(int argc, char *argv[])
 {
@@ -7,16 +11,17 @@ int main(int argc, char *argv[])
     mln_file_t *file;
 
     fset = mln_fileset_init(100);
-    if (fset == NULL) {
-        fprintf(stderr, "fset init failed\n");
-        return -1;
-    }
+    assert(fset != NULL);
 
-    file = mln_file_open(fset, "a.c");//打开本文件
-    if (file == NULL) {
-        fprintf(stderr, "file open failed\n");
-        return -1;
-    }
+    int fd = open("/tmp/aaa", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+    assert(fd >= 0);
+    int n = write(fd, "Hello", 5);
+    assert(n == 5);
+    close(fd);
+
+    file = mln_file_open(fset, "/tmp/aaa");
+    assert(file != NULL);
+
     printf("filename: %s size:%lu\n", file->file_path->data, (unsigned long)(file->size));
 
     mln_fileset_destroy(fset);
